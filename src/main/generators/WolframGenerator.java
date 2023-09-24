31d48beee70dd297f264060d09ab65aca2a76936
Kostia <47629852+kosssst@users.noreply.github.com>
2023-09-24 21:42:40 +0300

    added math util

--- src/main/generators/WolframGenerator.java
+++ src/main/generators/WolframGenerator.java
@@ -1,5 +1,7 @@
 package main.generators;
 
+import static main.util.MathUtil.*;
+
 public class WolframGenerator {
     private String seed;
     private final String ZEROS = "0".repeat(32);
@@ -18,39 +20,5 @@ public class WolframGenerator {
             seed = xor(cycleLeft(seed, 1), or(seed, cycleRight(seed, 1)));
         }
         return result.toString();
-    }
-
-    private String xor(String a, String b) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 32; i++) {
-            result.append((a.charAt(i) - '0') ^ (b.charAt(i) - '0'));
-        }
-        return result.toString();
-    }
-
-    private String or(String a, String b) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 32; i++) {
-            result.append((a.charAt(i) - '0') | (b.charAt(i) - '0'));
-        }
-        return result.toString();
-    }
-
-    private String cycleLeft(String a, int n) {
-        for (int i = 0; i < n; i++) {
-            char temp = a.charAt(0);
-            a = a.substring(1);
-            a += temp;
-        }
-        return a;
-    }
-
-    private String cycleRight(String a, int n) {
-        for (int i = 0; i < n; i++) {
-            char temp = a.charAt(31);
-            a = a.substring(0, 31);
-            a = temp + a;
-        }
-        return a;
     }
 }
