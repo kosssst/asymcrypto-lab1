5854473842148726b9a0cbb17c4a3a886f625de5
Kostia <47629852+kosssst@users.noreply.github.com>
2023-09-28 13:35:45 +0300

    added last tests, fixed librarian generator

--- src/main/generators/LibrarianGenerator.java
+++ src/main/generators/LibrarianGenerator.java
@@ -22,11 +22,8 @@ public class LibrarianGenerator {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < (int) Math.ceil((double) length / 8); i++) {
             result.append(String.format("%8s", Integer.toBinaryString((int) text.charAt(seed))).replace(" ", "0"));
-            if (seed != length) {
-                seed += 1;
-            } else {
-                seed = 0;
-            }
+            seed += 1;
+            if (seed == text.length()) seed = 0;
         }
         return result.substring(0, length);
     }
