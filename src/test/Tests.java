a618f78feff73d0de6bbc52d1df658a140038bee
Kostia <47629852+kosssst@users.noreply.github.com>
2023-10-02 18:08:09 +0300

    rewrote first test

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -9,13 +9,16 @@ import java.util.Map;
 public class Tests {
 
     public static boolean criterionOfUniformityOfBits(String sequence, double alpha) {
-        double n = (double) sequence.length() / 2;
-        int v0 = TextUtil.countMatches(sequence, '0');
-        int v1 = TextUtil.countMatches(sequence, '1');
-        double hi2 = (Math.pow(v0 - n, 2) / n) + (Math.pow(v1 - n, 2) / n);
-        NormalDistribution standardDistribution = new NormalDistribution();
-        double z = standardDistribution.inverseCumulativeProbability(1 - alpha);
-        double hi2Alpha = 2 * z + 2;
+        Map<String, Integer> bytes = TextUtil.getBytes(sequence);
+        double n = (double) sequence.length() / 256;
+        NormalDistribution normalDistribution = new NormalDistribution();
+
+        double hi2 = 0;
+        for (String b : TextUtil.bytes) {
+            hi2 += Math.pow(bytes.get(b) - n, 2) / n;
+        }
+
+        double hi2Alpha = Math.sqrt(2 * 255) * normalDistribution.inverseCumulativeProbability(1 - alpha) + 255;
 
         return hi2 <= hi2Alpha;
     }
